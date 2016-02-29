8c368618b4e18b1836e20d7ee1215e095c722491
LiShunyang <lishunyang@pku.edu.cn>
2016-02-29 11:24:43 +0800

    Add favicon and uncomment some code

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import path from 'path';
-// import favicon from 'serve-favicon';
-// import logger from 'morgan';
-// import cookieParser from 'cookie-parser';
-// import bodyParser from 'body-parser';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
@@ -18,11 +18,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const compiler = webpack(config);
